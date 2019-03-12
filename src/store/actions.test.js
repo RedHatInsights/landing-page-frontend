13252ee28551c712b677124346dcf21a6af0a4a6
Karel Hala <khala@redhat.com>
2019-03-12 15:37:34 +0100

    Add action test

---
+++ src/store/actions.test.js
@@ -0,0 +1,8 @@
+import { technologiesLoaded } from './actions';
+
+test('should fire correct action', () => {
+    expect(technologiesLoaded('some')).toMatchObject({
+        type: 'TECHNOLOGIES',
+        payload: 'some'
+    });
+});
