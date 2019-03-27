2f4eb9da64aaed651d923ee92292f8c50f94d510
Ryan Long <rlong@redhat.com>
2019-03-27 08:29:22 -0400

    removing custom paper plane icon for now

--- src/layout/FooterMenu.js
+++ src/layout/FooterMenu.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Grid, GridItem, TextContent, Text, TextVariants, Split, SplitItem } from '@patternfly/react-core';
-// import { OutlinedPaperPlaneIcon } from '@patternfly/react-icons';
-
-import Airplane from '../components/Airplane.js';
+import { Grid, GridItem, TextContent, Text, TextVariants, Split, SplitItem, List, ListItem } from '@patternfly/react-core';
+import { OutlinedPaperPlaneIcon } from '@patternfly/react-icons';
 
 import './FooterMenu.scss';
 
@@ -13,9 +11,11 @@ const FooterMenu = () => (
                 <TextContent>
                     <Text component={ TextVariants.h2 }>Support and resources</Text>
                 </TextContent>
-                <a href="#">View documentation</a>
-                <a href="#">Open a support case</a>
-                <a href="#">Contact customer service</a>
+                <List variant='grid'>
+                    <ListItem><a href="#">View documentation</a></ListItem>
+                    <ListItem><a href="#">Open a support case</a></ListItem>
+                    <ListItem><a href="#">Contact customer service</a></ListItem>
+                </List>
             </GridItem>
             <GridItem>
                 <TextContent>
@@ -31,7 +31,7 @@ const FooterMenu = () => (
                     <Text component={ TextVariants.h2 }>Feedback</Text>
                 </TextContent>
                 <Split gutter="md">
-                    <SplitItem><Airplane className="ins-c-icon__active"/></SplitItem>
+                    <SplitItem><OutlinedPaperPlaneIcon size='xl' className="ins-c-icon__active"/></SplitItem>
                     <SplitItem isMain><a href="#" className='ins-m-inline'>Tell us about your experience</a> using
                     Red Hat Cloud Services software, and how we can improve.</SplitItem>
                 </Split>
