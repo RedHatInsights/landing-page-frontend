07ffdaa5c2180cf4bcc4c5659551313832770bc5
Karel Hala <khala@redhat.com>
2019-04-11 15:03:52 +0200

    Remove console log

--- src/layout/Body.js
+++ src/layout/Body.js
@@ -22,69 +22,66 @@ import './Body.scss';
 // something like `window.insights.chrome.isBeta
 const isBeta = window.location.pathname.indexOf('/beta') === 0;
 
-const Body = ({ technologies }) => {
-    console.log(technologies, 'fff');
-    return (
-        <Fragment>
-            <Grid sm={ 12 } md={ 6 } lg={ isBeta ? 3 : 4 } gutter="md">
-                { technologies.map(({ icon: Icon, image, iconProps, title, url, body, isPreview, id }, key) => (
-                    <GridItem key={ key }>
-                        <a className='ins-c-card__link' href={ `./${url}` } aria-label={ `Go to ${title}` }>
-                            <Card className="ins-c-application-info" application-id={ id }>
-                                <CardHeader>
-                                    <Stack gutter='sm'>
-                                        <StackItem>
-                                            { image && <img
-                                                className="ins-c-application-info__logo"
-                                                aria-hidden
-                                                src={ image }
-                                                alt={ `${title} logo` } /> }
-                                            { Icon && <Icon
-                                                className="ins-c-application-info__logo ins-c-icon__active"
-                                                aria-hidden
-                                                size="xl"
-                                                alt={ `${title} logo` }
-                                                { ...iconProps } /> }
-                                        </StackItem>
-                                        <StackItem>
-                                            <Title headingLevel='h2' size='2xl'>
-                                                { title }
-                                            </Title>
-                                        </StackItem>
-                                    </Stack>
-                                </CardHeader>
-                                <CardBody>
-                                    <Stack>
-                                        { isPreview &&
+const Body = ({ technologies }) => (
+    <Fragment>
+        <Grid sm={ 12 } md={ 6 } lg={ isBeta ? 3 : 4 } gutter="md">
+            { technologies.map(({ icon: Icon, image, iconProps, title, url, body, isPreview, id }, key) => (
+                <GridItem key={ key }>
+                    <a className='ins-c-card__link' href={ `./${url}` } aria-label={ `Go to ${title}` }>
+                        <Card className="ins-c-application-info" application-id={ id }>
+                            <CardHeader>
+                                <Stack gutter='sm'>
+                                    <StackItem>
+                                        { image && <img
+                                            className="ins-c-application-info__logo"
+                                            aria-hidden
+                                            src={ image }
+                                            alt={ `${title} logo` } /> }
+                                        { Icon && <Icon
+                                            className="ins-c-application-info__logo ins-c-icon__active"
+                                            aria-hidden
+                                            size="xl"
+                                            alt={ `${title} logo` }
+                                            { ...iconProps } /> }
+                                    </StackItem>
+                                    <StackItem>
+                                        <Title headingLevel='h2' size='2xl'>
+                                            { title }
+                                        </Title>
+                                    </StackItem>
+                                </Stack>
+                            </CardHeader>
+                            <CardBody>
+                                <Stack>
+                                    { isPreview &&
                                         <StackItem>
                                             <div className="ins-m-tech-preview">
                                                 <BinocularsIcon size="sm" /> Tech Preview
                                             </div>
                                         </StackItem>
-                                        }
-                                        <StackItem>
-                                            <span className='ins-m-gray'>{ body }</span>
-                                        </StackItem>
-                                    </Stack>
-                                </CardBody>
-                                <CardFooter>
-                                    <Split gutter="sm" className="ins-c-open-card">
-                                        <SplitItem>
+                                    }
+                                    <StackItem>
+                                        <span className='ins-m-gray'>{ body }</span>
+                                    </StackItem>
+                                </Stack>
+                            </CardBody>
+                            <CardFooter>
+                                <Split gutter="sm" className="ins-c-open-card">
+                                    <SplitItem>
                                         Open
-                                        </SplitItem>
-                                        <SplitItem>
-                                            <ArrowRightIcon size="sm" />
-                                        </SplitItem>
-                                    </Split>
-                                </CardFooter>
-                            </Card>
-                        </a>
-                    </GridItem>
-                )) }
-            </Grid>
-        </Fragment>
-    );
-};
+                                    </SplitItem>
+                                    <SplitItem>
+                                        <ArrowRightIcon size="sm" />
+                                    </SplitItem>
+                                </Split>
+                            </CardFooter>
+                        </Card>
+                    </a>
+                </GridItem>
+            )) }
+        </Grid>
+    </Fragment>
+);
 
 Body.propTypes = {
     technologies: PropTypes.arrayOf(PropTypes.shape({
