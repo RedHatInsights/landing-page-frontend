b666a0e6c5764432baf1dfe12ec3d754461c2a7d
Ryan Long <rlong@redhat.com>
2019-05-03 09:09:13 -0400

    adding logout page (#37)
    * adding logout page
    
    * swapping onclick for href
    
    * updating again

--- src/routes/Logout.js
+++ src/routes/Logout.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Title, Button } from '@patternfly/react-core';
+
+import './Logout.scss';
 
 const Logout = () => {
     return (
-        <section page-type="" className="pf-l-page__main-section pf-c-page__main-section">
-            Logout
+        <section className="pf-l-page__main-section pf-c-page__main-section ins-c-page__logout">
+            <Title size='3xl'> You have logged out of Cloud Services </Title>
+            <Button variant='link' onClick={ () => window.insights.chrome.auth.login() }>Log back in</Button>
         </section>
     );
 };
