d254c921fda4b96c4a2f8592151fabd1234fd55a
Ryan Long <rlong@redhat.com>
2019-05-03 15:36:31 -0400

    redirect to home for now (#41)
    * redirect to home for now
    
    * lint

--- src/routes/Logout.js
+++ src/routes/Logout.js
@@ -3,11 +3,15 @@ import { Title, Button } from '@patternfly/react-core';
 
 import './Logout.scss';
 
+const onButtonClick = (url) => {
+    window.location.href = `./${url}`;
+};
+
 const Logout = () => {
     return (
         <section className="pf-l-page__main-section pf-c-page__main-section ins-c-page__logout">
             <Title size='3xl'> You have logged out of Cloud Services </Title>
-            <Button variant='link' onClick={ () => window.insights.chrome.auth.login() }>Log back in</Button>
+            <Button variant='link' onClick={ () => onButtonClick('') }>Log back in</Button>
         </section>
     );
 };
