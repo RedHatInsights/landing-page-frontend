fe9391d4530d2429a4ae3cc4639f8e55bf74c1e9
Karel Hala <khala@redhat.com>
2019-07-09 13:45:21 +0200

    Remove prod pushes when merged to master

--- .travis/custom_release.sh
+++ .travis/custom_release.sh
@@ -4,7 +4,7 @@ set -x
 
 if [ "${TRAVIS_BRANCH}" = "master" ]
 then
-    for env in ci qa prod
+    for env in ci qa
     do
         echo "PUSHING ${env}-beta"
         rm -rf ./dist/.git
@@ -15,10 +15,16 @@ fi
 
 if [ "${TRAVIS_BRANCH}" = "master-stable" ]
 then
-    for env in ci qa prod
+    for env in ci qa
     do
         echo "PUSHING ${env}-stable"
         rm -rf ./dist/.git
         .travis/release.sh "${env}-stable"
     done
+fi
+
+if [[ "${TRAVIS_BRANCH}" = "prod-beta" || "${TRAVIS_BRANCH}" = "prod-stable" ]]; then
+    echo "PUSHING ${TRAVIS_BRANCH}"
+    rm -rf ./build/.git
+    .travis/release.sh "${TRAVIS_BRANCH}"
 fi
