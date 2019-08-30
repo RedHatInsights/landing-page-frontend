cf30276777c81a42cc9bf442b401d84a90b01b3f
Yash Nalla <yashnalla98@gmail.com>
2019-08-30 14:02:02 -0400

    add prop size to tite

--- src/layout/Hero.js
+++ src/layout/Hero.js
@@ -7,8 +7,9 @@ import './Hero.scss';
 const Hero = () => (
     <PageSection className='ins-c-hero pf-m-fill pf-l-flex pf-m-align-items-center pf-m-justify-content-center'>
         <article className='pf-l-flex pf-m-column pf-m-align-items-center'>
-            <Title className='ins-c-hero__title pf-m-spacer-lg'> Explore our open, multicloud tools </Title>
-            <Title headingLevel='h2' className='pf-m-spacer-xl'> Discover Red Hat<sup className='ins-c-rball'>®</sup> software-as-a-service </Title>
+            <Title size='md' className='ins-c-hero__title pf-m-spacer-lg'> Explore our open, multicloud tools </Title>
+            <Title size='md' headingLevel='h2' className='pf-m-spacer-xl'> Discover Red Hat<sup className='ins-c-rball'>®</sup>
+            software-as-a-service </Title>
             <Button onClick={ () => window.insights.chrome.auth.login() }> Log in to your Red Hat account</Button>
             <a href='https://www.redhat.com/en/customers'> Not a customer? </a>
         </article>
