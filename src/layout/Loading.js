31347a72ceaedb9eda547a6ef0d6aea7f7b64066
Ryan Long <rlong@redhat.com>
2019-09-30 13:38:44 -0400

    add a loading page when waiting for token

---
+++ src/layout/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Main, Spinner } from '@red-hat-insights/insights-frontend-components';
+
+const Loading = () => (
+    <Main>
+        <Spinner centered/>
+    </Main>
+);
+
+export default Loading;
