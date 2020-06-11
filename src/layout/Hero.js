c9c1928e1752105b0e0c16968e01bedd4cce6fe4
Ryan Long <rlong@redhat.com>
2020-06-11 09:33:11 -0400

    add 4xl back

--- src/layout/Hero.js
+++ src/layout/Hero.js
@@ -7,7 +7,7 @@ import './Hero.scss';
 const Hero = () => (
     <PageSection className='ins-c-hero pf-m-fill pf-l-flex pf-m-align-items-center pf-m-justify-content-center'>
         <article className='pf-l-flex pf-m-column pf-m-align-items-center'>
-            <Title headingLevel='h1' className='ins-c-hero__title pf-m-spacer-lg'> Explore our open, multicloud tools </Title>
+            <Title headingLevel='h1' size='4xl' className='ins-c-hero__title pf-m-spacer-lg'> Explore our open, multicloud tools </Title>
             <Title headingLevel='h2' className='ins-c-hero__sub-title pf-m-spacer-xl'>
                 Discover Red Hat<sup className='ins-c-rball'>®</sup> software-as-a-service
             </Title>
