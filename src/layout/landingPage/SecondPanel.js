51aff6bfd3c21e183a0693cb9f992006ea1d603a
Eric Winchell <1287144+epwinchell@users.noreply.github.com>
2021-01-20 15:33:29 -0500

    estate cleanup (#183)

--- src/layout/landingPage/SecondPanel.js
+++ src/layout/landingPage/SecondPanel.js
@@ -1,76 +1,67 @@
 import React from 'react';
-import { Title, Flex, FlexItem } from '@patternfly/react-core';
+import { Flex, FlexItem, Text, Title } from '@patternfly/react-core';
 import SecondPanelTile from './SecondPanelTile';
-import './styles/SecondPanel.scss';
 
 const SecondPanel = () => {
   return (
-    <Flex className="ins-p-console-landing-second-level" hasGutter>
-      <FlexItem>
-        <Flex>
-          <FlexItem className="ins-c-landing-grouping__title">
-            <Title headingLevel="h1" size="xl">
-              Application Services Recommendations
-            </Title>
-          </FlexItem>
+    <Flex className="second-level">
+      <Flex className="sections">
+        <Flex className="subsection">
           <FlexItem>
+            <Text component="h1">Application Services Recommendations</Text>
+          </FlexItem>
+          <FlexItem className="tile-group">
+            <SecondPanelTile />
             <SecondPanelTile />
           </FlexItem>
-          <FlexItem className="ins-c-landing-grouping--separator">
-            <SecondPanelTile />
-          </FlexItem>
-          <br />
-          <FlexItem className="ins-c-landing-grouping__title">
-            <Title headingLevel="h1" size="xl">
+        </Flex>
+        <Flex className="subsection">
+          <FlexItem>
+            <Text component="h1" className="title">
               Automation recommendations
-            </Title>
+            </Text>
           </FlexItem>
-          <FlexItem>
+          <FlexItem className="tile-group">
             <SecondPanelTile />
-          </FlexItem>
-          <FlexItem>
-            <SecondPanelTile />
-          </FlexItem>
-        </Flex>
-      </FlexItem>
-
-      <FlexItem>
-        <Flex>
-          <FlexItem className="ins-c-landing-grouping__title">
-            <Title headingLevel="h1" size="xl">
-              Platform insights
-            </Title>
-          </FlexItem>
-          <FlexItem>
-            <SecondPanelTile title="Operational" />
-          </FlexItem>
-          <FlexItem>
-            <SecondPanelTile />
-          </FlexItem>
-          <FlexItem>
-            <SecondPanelTile title="Financial" />
-          </FlexItem>
-          <FlexItem>
             <SecondPanelTile />
           </FlexItem>
         </Flex>
-      </FlexItem>
-
-      <FlexItem>
-        <Flex>
-          <FlexItem className="ins-c-landing-grouping__title">
-            <Title headingLevel="h1" size="xl">
-              &nbsp;
+      </Flex>
+      <Flex className="sections">
+        <Flex className="subsection">
+          <FlexItem>
+            <Text component="h1">Platform insights</Text>
+          </FlexItem>
+          <FlexItem className="tile-group">
+            <Title headingLevel="h2" size="md">
+              Operational
             </Title>
+            <SecondPanelTile />
+            <SecondPanelTile />
           </FlexItem>
-          <FlexItem>
-            <SecondPanelTile title="Security" />
-          </FlexItem>
-          <FlexItem>
+          <FlexItem className="tile-group">
+            <Title headingLevel="h2" size="md">
+              Financial
+            </Title>
+            <SecondPanelTile />
             <SecondPanelTile />
           </FlexItem>
         </Flex>
-      </FlexItem>
+      </Flex>
+      <Flex className="sections">
+        <Flex className="subsection">
+          <FlexItem>
+            <Text component="h1">&nbsp;</Text>
+          </FlexItem>
+          <FlexItem className="tile-group">
+            <Title headingLevel="h2" size="md">
+              Security
+            </Title>
+            <SecondPanelTile />
+            <SecondPanelTile />
+          </FlexItem>
+        </Flex>
+      </Flex>
     </Flex>
   );
 };
