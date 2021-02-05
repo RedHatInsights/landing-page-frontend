6b6471ff33d56d9cfd548887ca316fd763126513
ewinchel <ewinchel@redhat.com>
2021-02-05 15:19:56 -0500

    Landing page refactor 4

--- src/layout/landingPage/SecondPanel.js
+++ src/layout/landingPage/SecondPanel.js
@@ -5,61 +5,63 @@ import SecondPanelTile from './SecondPanelTile';
 const SecondPanel = () => {
   return (
     <Flex className="second-level">
-      <Flex className="section" hasGutter>
-        <Flex className="subsection">
-          <FlexItem>
-            <Text component="h1">Application Services Recommendations</Text>
-          </FlexItem>
-          <FlexItem className="tile-group">
-            <SecondPanelTile />
-            <SecondPanelTile />
-          </FlexItem>
+      <Flex className="level-wrapper">
+        <Flex className="section">
+          <Flex className="subsection">
+            <FlexItem>
+              <Text component="h1">Application Services Recommendations</Text>
+            </FlexItem>
+            <FlexItem className="tile-group">
+              <SecondPanelTile />
+              <SecondPanelTile />
+            </FlexItem>
+          </Flex>
+          <Flex className="subsection">
+            <FlexItem>
+              <Text component="h1" className="title">
+                Automation recommendations
+              </Text>
+            </FlexItem>
+            <FlexItem className="tile-group">
+              <SecondPanelTile />
+              <SecondPanelTile />
+            </FlexItem>
+          </Flex>
         </Flex>
-        <Flex className="subsection">
-          <FlexItem>
-            <Text component="h1" className="title">
-              Automation recommendations
-            </Text>
-          </FlexItem>
-          <FlexItem className="tile-group">
-            <SecondPanelTile />
-            <SecondPanelTile />
-          </FlexItem>
-        </Flex>
-      </Flex>
-      <Flex className="section">
-        <Flex className="subsection">
-          <FlexItem>
-            <Text component="h1">Platform insights</Text>
-          </FlexItem>
-          <FlexItem className="tile-group">
-            <Title headingLevel="h2" size="md">
-              Operational
-            </Title>
-            <SecondPanelTile />
-            <SecondPanelTile />
-          </FlexItem>
-          <FlexItem className="tile-group">
-            <Title headingLevel="h2" size="md">
-              Financial
-            </Title>
-            <SecondPanelTile />
-            <SecondPanelTile />
-          </FlexItem>
-        </Flex>
-      </Flex>
-      <Flex className="section">
-        <Flex className="subsection">
-          <FlexItem>
-            <Text component="h1">&nbsp;</Text>
-          </FlexItem>
-          <FlexItem className="tile-group">
-            <Title headingLevel="h2" size="md">
-              Security
-            </Title>
-            <SecondPanelTile />
-            <SecondPanelTile />
-          </FlexItem>
+        <Flex className="section">
+          <Flex className="subsection">
+            <FlexItem>
+              <Text component="h1">Platform insights</Text>
+            </FlexItem>
+            <FlexItem className="tile-group">
+              <Title headingLevel="h2" size="md">
+                Operational
+              </Title>
+              <SecondPanelTile />
+              <SecondPanelTile />
+            </FlexItem>
+            <FlexItem className="tile-group">
+              <Title headingLevel="h2" size="md">
+                Financial
+              </Title>
+              <SecondPanelTile />
+              <SecondPanelTile />
+            </FlexItem>
+          </Flex>
+          <Flex className="subsection">
+            <FlexItem>
+              <Text component="h1" className="security">
+                .
+              </Text>
+            </FlexItem>
+            <FlexItem className="tile-group">
+              <Title headingLevel="h2" size="md">
+                Security
+              </Title>
+              <SecondPanelTile />
+              <SecondPanelTile />
+            </FlexItem>
+          </Flex>
         </Flex>
       </Flex>
     </Flex>
