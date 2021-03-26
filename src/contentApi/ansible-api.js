ef2c33f943a25ab099c54b4f8483598344b5880f
Martin Marosi <marvusm.mmi@gmail.com>
2021-03-26 11:05:42 +0100

    Generalize ansible shcema.

--- src/contentApi/ansible-api.js
+++ src/contentApi/ansible-api.js
@@ -1,180 +1,4 @@
-/* eslint-disable no-empty */
-import { instance } from '@redhat-cloud-services/frontend-components-utilities/interceptors';
-
-const createNoDataResponse = (error, title, href) => {
-  if (error?.error === 'No data') {
-    return {
-      title,
-      count: 0,
-      href,
-    };
-  }
-
-  return undefined;
-};
-
-const CLUSTERS_TITLE = 'Ansible Platform Clusters';
-const PAST_WEEK_JOBS_TITLE = 'Jobs in the past week';
-const MANAGED_HOSTS_TITLE = 'Ansible managed hosts';
-
-export const getAnsiblePlatformClusters = async () => {
-  try {
-    const clusters = await instance.get('/api/tower-analytics/v0/clusters/');
-    const { templates } = clusters;
-    return {
-      title: CLUSTERS_TITLE,
-      count: templates.length,
-      href: './ansible/automation-analytics/clusters',
-    };
-  } catch (error) {
-    return createNoDataResponse(
-      error,
-      CLUSTERS_TITLE,
-      './ansible/automation-analytics/clusters'
-    );
-  }
-};
-
-export const getPastWeekJobs = async () => {
-  const href =
-    'https://cloud.redhat.com/ansible/automation-analytics/job-explorer?attributes[]=id&attributes[]=status&attributes[]=job_type&attributes[]=started&attributes[]=finished&attributes[]=elapsed&attributes[]=created&attributes[]=cluster_name&attributes[]=org_name&attributes[]=most_failed_tasks&limit=5&quick_date_range=last_30_days&sort_by=created%3Adesc';
-  try {
-    const {
-      data: { total_count },
-    } = await instance.post('/api/tower-analytics/v1/job_explorer_summary/', {
-      quick_date_range: 'last_week',
-      attributes: ['total_count'],
-    });
-    return {
-      title: PAST_WEEK_JOBS_TITLE,
-      count: total_count,
-      href,
-    };
-  } catch (error) {
-    return createNoDataResponse(error, PAST_WEEK_JOBS_TITLE, href);
-  }
-};
-
-export const getManagedHosts = async () => {
-  try {
-    const {
-      data: { total_unique_host_count },
-    } = await instance.post('/api/tower-analytics/v1/host_explorer_summary/', {
-      quick_date_range: 'last_2_years',
-      granularity: 'yearly',
-      attributes: ['total_unique_host_count'],
-    });
-    return {
-      count: total_unique_host_count,
-      title: MANAGED_HOSTS_TITLE,
-    };
-  } catch (error) {
-    return createNoDataResponse(error, MANAGED_HOSTS_TITLE);
-  }
-};
-
-export const getAPPAnalyticsUserEstateItems = () =>
-  Promise.all([
-    getAnsiblePlatformClusters(),
-    getPastWeekJobs(),
-    getManagedHosts(),
-  ]).then((items) => items.filter((item) => typeof item !== 'undefined'));
-
-const APP_USER_CONFIGRE_TRY_LEARN = {
-  configure: [
-    {
-      icon: 'unknown',
-      title: 'Connect your Ansible platform clusters to Analytics',
-      description: 'Send Analytics data to the cloud hosted services ',
-      link: {
-        title: 'Configure',
-        href:
-          'https://docs.ansible.com/ansible-tower/latest/html/administration/usability_data_collection.html',
-      },
-    },
-    {
-      icon: 'unknown',
-      title: 'Setup user access',
-      link: {
-        title: 'Create',
-        href: './settings/rbac/users',
-      },
-    },
-  ],
-  try: [
-    {
-      icon: 'unknown',
-      title: 'Calculate your automation savings',
-      link: {
-        title: 'Calculate',
-        href: './ansible/automation-analytics/automation-calculator',
-      },
-    },
-    {
-      icon: 'unknown',
-      title: 'Tour Job Explorer',
-      link: {
-        title: 'View',
-        href:
-          './ansible/automation-analytics/job-explorer?attributes[]=id&attributes[]=status&attributes[]=job_type&attributes[]=started&attributes[]=finished&attributes[]=elapsed&attributes[]=created&attributes[]=cluster_name&attributes[]=org_name&attributes[]=most_failed_tasks&limit=5',
-      },
-    },
-  ],
-  learn: [
-    {
-      icon: 'unknown',
-      title: 'Learn more about Automation Analytics',
-      link: {
-        title: 'Learn more',
-        href: './ansible/automation-analytics/automation-calculator',
-      },
-    },
-  ],
-};
-
-export const getAPPAnalyticsUserConfigureTryLearn = () =>
-  Promise.resolve(APP_USER_CONFIGRE_TRY_LEARN);
-
-export const getAPPAnalyticsUserData = () =>
-  Promise.all([
-    getAPPAnalyticsUserEstateItems(),
-    getAPPAnalyticsUserConfigureTryLearn(),
-  ]).then(([estate, configTryLear]) => ({
-    estate,
-    recommendations: undefined, // does not have this section
-    configTryLear,
-  }));
-
-export const getCollections = async () => {
-  try {
-    const {
-      meta: { count },
-    } = await instance.get('/api/automation-hub/v3/collections');
-    return {
-      title: 'Collections',
-      count,
-      href: './ansible/automation-hub/',
-    };
-  } catch (error) {}
-};
-
-export const getPartners = async () => {
-  try {
-    const {
-      meta: { count },
-    } = await instance.get('/api/automation-hub/v3/namespaces');
-    return {
-      title: 'Partners',
-      count,
-      href: './ansible/automation-hub/partners',
-    };
-  } catch (error) {}
-};
-
-export const getHubUserEstateItems = () =>
-  Promise.all([getCollections(), getPartners()]).then((items) =>
-    items.filter((item) => typeof item !== 'undefined')
-  );
+import processRequest from './request-processor';
 
 const HUB_USER_CONFIGRE_TRY_LEARN = {
   configure: [
@@ -253,90 +77,59 @@ const HUB_USER_CONFIGRE_TRY_LEARN = {
   ],
 };
 
-export const getHubUserConfigureTryLearn = () =>
-  Promise.resolve(HUB_USER_CONFIGRE_TRY_LEARN);
-
-export const getHubUserData = () =>
-  Promise.all([getHubUserEstateItems(), getHubUserConfigureTryLearn()]).then(
-    ([estate, configTryLear]) => ({
-      estate,
-      recommendations: undefined, // does not have this section
-      configTryLear,
-    })
-  );
-
-export const getCatalogPortfolios = async () => {
-  try {
-    const {
-      meta: { count },
-    } = await instance.get('/api/catalog/v1/portfolios?limit=1');
-    return {
-      count,
-      section: 'Catalog', // don't know no data
-      title: 'Portfolios',
-      href: './ansible/catalog/portfolios',
-    };
-  } catch (error) {}
-};
-
-export const getCatalogProducts = async () => {
-  try {
-    const {
-      meta: { count },
-    } = await instance.get('/api/catalog/v1/portfolio_items?limit=1');
-    return {
-      count,
-      title: 'Products',
-      href: './ansible/catalog/products',
-    };
-  } catch (error) {}
-};
-
-export const getCatalogAdminEstateItems = () =>
-  Promise.all([getCatalogPortfolios(), getCatalogProducts()]).then((items) =>
-    items.filter((item) => typeof item !== 'undefined')
-  );
-
-export const getCatalogAdminRecommendationsItems = async () =>
-  Promise.resolve([
+const APP_USER_CONFIGRE_TRY_LEARN = {
+  configure: [
     {
-      title: 'Catalog',
-      id: 'catalog',
-      groups: [
-        {
-          id: 'products',
-          description: `Last added products (5)`,
-          icon: 'unknown',
-          action: {
-            url:
-              'https://cloud.stage.redhat.com/docs/api/catalog#operations-PortfolioItem-listPortfolioItems',
-            title: 'Products',
-          },
-        },
-        {
-          id: 'orders',
-          description: `Last orders (5)`,
-          icon: 'unknown',
-          action: {
-            url:
-              'https://cloud.stage.redhat.com/docs/api/catalog#operations-Order-listOrders',
-            title: 'Orders',
-          },
-        },
-        {
-          id: 'approvals',
-          description: `Approvals`,
-          icon: 'unknown',
-          action: {
-            url: './ansible/catalog/approval/requests',
-            title: 'Approvals',
-          },
-        },
-      ],
+      icon: 'unknown',
+      title: 'Connect your Ansible platform clusters to Analytics',
+      description: 'Send Analytics data to the cloud hosted services ',
+      link: {
+        title: 'Configure',
+        href:
+          'https://docs.ansible.com/ansible-tower/latest/html/administration/usability_data_collection.html',
+      },
     },
-  ]);
+    {
+      icon: 'unknown',
+      title: 'Setup user access',
+      link: {
+        title: 'Create',
+        href: './settings/rbac/users',
+      },
+    },
+  ],
+  try: [
+    {
+      icon: 'unknown',
+      title: 'Calculate your automation savings',
+      link: {
+        title: 'Calculate',
+        href: './ansible/automation-analytics/automation-calculator',
+      },
+    },
+    {
+      icon: 'unknown',
+      title: 'Tour Job Explorer',
+      link: {
+        title: 'View',
+        href:
+          './ansible/automation-analytics/job-explorer?attributes[]=id&attributes[]=status&attributes[]=job_type&attributes[]=started&attributes[]=finished&attributes[]=elapsed&attributes[]=created&attributes[]=cluster_name&attributes[]=org_name&attributes[]=most_failed_tasks&limit=5',
+      },
+    },
+  ],
+  learn: [
+    {
+      icon: 'unknown',
+      title: 'Learn more about Automation Analytics',
+      link: {
+        title: 'Learn more',
+        href: './ansible/automation-analytics/automation-calculator',
+      },
+    },
+  ],
+};
 
-const CATALOG_ADMIN_CONFIGRE_TRY_LEARN = {
+const CATALOG_ADMIN_CONFIGuRE_TRY_LEARN = {
   configure: [
     {
       icon: 'unknown',
@@ -402,29 +195,174 @@ const CATALOG_ADMIN_CONFIGRE_TRY_LEARN = {
   ],
 };
 
-export const getCatalogConfigureTryLearn = () =>
-  Promise.resolve(CATALOG_ADMIN_CONFIGRE_TRY_LEARN);
+const CONFIGURE_TRY_LEARN = {
+  configure: [
+    ...HUB_USER_CONFIGRE_TRY_LEARN.configure,
+    ...APP_USER_CONFIGRE_TRY_LEARN.configure,
+    ...CATALOG_ADMIN_CONFIGuRE_TRY_LEARN.configure,
+  ],
+  try: [
+    ...HUB_USER_CONFIGRE_TRY_LEARN.try,
+    ...APP_USER_CONFIGRE_TRY_LEARN.try,
+    ...CATALOG_ADMIN_CONFIGuRE_TRY_LEARN.try,
+  ],
+  learn: [
+    ...HUB_USER_CONFIGRE_TRY_LEARN.learn,
+    ...APP_USER_CONFIGRE_TRY_LEARN.learn,
+    ...CATALOG_ADMIN_CONFIGuRE_TRY_LEARN.learn,
+  ],
+};
 
-export const getCatalogAdminData = () =>
+const createNoDataResponse = (error, shape) => {
+  if (error?.error === 'No data') {
+    return {
+      ...shape,
+      count: 0,
+    };
+  }
+
+  return undefined;
+};
+
+const CLUSTERS_TITLE = 'Ansible Platform Clusters';
+const PAST_WEEK_JOBS_TITLE = 'Jobs in the past week';
+const MANAGED_HOSTS_TITLE = 'Ansible managed hosts';
+
+const ansibleEstateRequests = [
+  {
+    url: '/api/tower-analytics/v0/clusters/',
+    accessor: 'templates.length',
+    shape: {
+      section: 'AAP',
+      title: CLUSTERS_TITLE,
+      href: './ansible/automation-analytics/clusters',
+    },
+    errorProcessor: createNoDataResponse,
+  },
+  {
+    url: '/api/tower-analytics/v1/job_explorer_summary/',
+    accessor: 'data.total_count',
+    method: 'post',
+    args: [
+      {
+        quick_date_range: 'last_week',
+        attributes: ['total_count'],
+      },
+    ],
+    shape: {
+      title: PAST_WEEK_JOBS_TITLE,
+      href:
+        'https://cloud.redhat.com/ansible/automation-analytics/job-explorer?attributes[]=id&attributes[]=status&attributes[]=job_type&attributes[]=started&attributes[]=finished&attributes[]=elapsed&attributes[]=created&attributes[]=cluster_name&attributes[]=org_name&attributes[]=most_failed_tasks&limit=5&quick_date_range=last_30_days&sort_by=created%3Adesc',
+    },
+    errorProcessor: createNoDataResponse,
+  },
+  {
+    url: '/api/tower-analytics/v1/host_explorer_summary/',
+    method: 'post',
+    accessor: 'data.total_unique_host_count',
+    args: [
+      {
+        quick_date_range: 'last_2_years',
+        granularity: 'yearly',
+        attributes: ['total_unique_host_count'],
+      },
+    ],
+    shape: {
+      title: MANAGED_HOSTS_TITLE,
+    },
+    errorProcessor: createNoDataResponse,
+  },
+  {
+    url: '/api/automation-hub/v3/collections',
+    accessor: 'meta.count',
+    shape: {
+      title: 'Collections',
+      href: './ansible/automation-hub/',
+    },
+  },
+  {
+    url: '/api/automation-hub/v3/namespaces',
+    accessor: 'meta.count',
+    shape: {
+      title: 'Partners',
+      href: './ansible/automation-hub/partners',
+    },
+  },
+  {
+    url: '/api/catalog/v1/portfolios?limit=1',
+    accessor: 'meta.count',
+    shape: {
+      section: 'Catalog', // don't know no data
+      title: 'Portfolios',
+      href: './ansible/catalog/portfolios',
+    },
+  },
+  {
+    url: '/api/catalog/v1/portfolio_items?limit=1',
+    accessor: 'meta.count',
+    shape: {
+      title: 'Products',
+      href: './ansible/catalog/products',
+    },
+  },
+];
+
+export const RECOMMENDATIONS_ITEMS = [
+  {
+    title: 'Ansible recommendations',
+    id: 'ansiblerrecommendations',
+    sections: [
+      {
+        title: 'Catalog',
+        id: 'catalog',
+        groups: [
+          {
+            id: 'products',
+            description: `Last added products (5)`,
+            icon: 'unknown',
+            action: {
+              url:
+                'https://cloud.stage.redhat.com/docs/api/catalog#operations-PortfolioItem-listPortfolioItems',
+              title: 'Products',
+            },
+          },
+          {
+            id: 'orders',
+            description: `Last orders (5)`,
+            icon: 'unknown',
+            action: {
+              url:
+                'https://cloud.stage.redhat.com/docs/api/catalog#operations-Order-listOrders',
+              title: 'Orders',
+            },
+          },
+          {
+            id: 'approvals',
+            description: `Approvals`,
+            icon: 'unknown',
+            action: {
+              url: './ansible/catalog/approval/requests',
+              title: 'Approvals',
+            },
+          },
+        ],
+      },
+    ],
+  },
+];
+
+export const getEstateItems = () =>
+  Promise.all(ansibleEstateRequests.map(processRequest)).then((items) =>
+    items.filter((item) => typeof item !== 'undefined')
+  );
+
+export const getAnsibleDataSchema = () =>
   Promise.all([
-    getCatalogAdminEstateItems(),
-    getCatalogAdminRecommendationsItems(),
-    getCatalogConfigureTryLearn(),
+    getEstateItems(),
+    Promise.resolve(RECOMMENDATIONS_ITEMS),
+    Promise.resolve(CONFIGURE_TRY_LEARN),
   ]).then(([estate, recommendations, configTryLearn]) => ({
-    estate,
-    recommendations,
+    firstPanel: estate,
+    secondPanel: recommendations,
     configTryLearn,
   }));
-
-export const createAnsibleSchema = () => {
-  /**
-   * We don't know how to switch personas yet. Using catalog for now, because they have the most data.
-   */
-  return getCatalogAdminData().then(
-    ({ estate, recommendations, configTryLearn }) => ({
-      firstPanel: estate,
-      secondPanel: recommendations,
-      configTryLearn,
-    })
-  );
-};
