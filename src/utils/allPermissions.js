6c93d6c260564a1aa3adb476919fddf87e4e2c98
rvsia <r.vsia@seznam.cz>
2021-03-30 11:18:44 +0200

    Retry the permission 5x to catch errors

--- src/utils/allPermissions.js
+++ src/utils/allPermissions.js
@@ -1,8 +1,14 @@
 let permissions = [];
 
-export const loadPermissions = async () => {
-  const userPermissions = await window.insights.chrome.getUserPermissions();
-  permissions = userPermissions.map(({ permission }) => permission);
+export const loadPermissions = async (retries = 5) => {
+  try {
+    if (retries > 0) {
+      const userPermissions = await window.insights.chrome.getUserPermissions();
+      permissions = userPermissions.map(({ permission }) => permission);
+    }
+  } catch {
+    await loadPermissions(retries - 1);
+  }
 };
 
 export const hasPermissions = (requestedPermissions = []) =>
