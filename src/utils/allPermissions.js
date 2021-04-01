810acf9d6fc61245670ffeaa1617d2367085eb3c
rvsia <r.vsia@seznam.cz>
2021-04-01 10:31:08 +0200

    Check permission is an array

--- src/utils/allPermissions.js
+++ src/utils/allPermissions.js
@@ -4,11 +4,9 @@ export const loadPermissions = async (retries = 5) => {
   try {
     if (retries > 0) {
       const userPermissions = await window.insights.chrome.getUserPermissions();
-      if (Array.isArray(userPermissions)) {
-        permissions = userPermissions.map(({ permission }) => permission);
-      }
+      permissions = userPermissions?.map(({ permission }) => permission) || [];
     }
-  } catch (e) {
+  } catch {
     await loadPermissions(retries - 1);
   }
 };
