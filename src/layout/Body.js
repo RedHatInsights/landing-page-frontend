645737528052ec456786acd531fc6a4f150a3020
rvsia <r.vsia@seznam.cz>
2021-04-01 17:03:49 +0200

    Add missing filtering from mapStateToProps

--- src/layout/Body.js
+++ src/layout/Body.js
@@ -58,131 +58,133 @@ const Body = () => {
       needs-rbac-tour={permission.isOrgAdmin ? `${needsRBACTour}` : 'false'}
     >
       <Grid md={6} lg={4} xl={3} hasGutter>
-        {technologies.map((
-          {
-            icon: Icon,
-            image,
-            iconProps,
-            title,
-            url,
-            apps,
-            baseApp,
-            body,
-            isPreview,
-            isEarlyAccess,
-            isDevPreview,
-            isUnderMaintenance,
-            isUnderMaintenanceApps,
-            id,
-          },
-          key // eslint-disable-line max-len
-        ) => (
-          <GridItem key={key} className="ins-c-application-card">
-            <Card
-              className="ins-c-application-info"
-              application-id={id}
-              ouiaId={title.toLowerCase().replace(' ', '_')}
-            >
-              <CardHeader>
-                <Stack>
-                  <StackItem>
-                    {image && (
-                      <img
-                        className="ins-c-application-info__logo"
-                        aria-hidden
-                        src={image}
-                        alt={`${title} logo`}
-                      />
-                    )}
-                    {Icon && (
-                      <Icon
-                        className="ins-c-application-info__logo ins-c-icon__active"
-                        aria-hidden
-                        size="xl"
-                        alt={`${title} logo`}
-                        {...iconProps}
-                      />
-                    )}
-                  </StackItem>
-                  <StackItem>
-                    <Title headingLevel="h2">{title}</Title>
-                  </StackItem>
-                </Stack>
-              </CardHeader>
-              <CardBody>
-                <Stack hasGutter className="ins-c-application-info__content">
-                  {isPreview && (
+        {technologies
+          .filter(({ disabled }) => !disabled)
+          .map((
+            {
+              icon: Icon,
+              image,
+              iconProps,
+              title,
+              url,
+              apps,
+              baseApp,
+              body,
+              isPreview,
+              isEarlyAccess,
+              isDevPreview,
+              isUnderMaintenance,
+              isUnderMaintenanceApps,
+              id,
+            },
+            key // eslint-disable-line max-len
+          ) => (
+            <GridItem key={key} className="ins-c-application-card">
+              <Card
+                className="ins-c-application-info"
+                application-id={id}
+                ouiaId={title.toLowerCase().replace(' ', '_')}
+              >
+                <CardHeader>
+                  <Stack>
                     <StackItem>
-                      <div className="ins-m-tech-preview">
-                        <BinocularsIcon size="sm" /> Tech preview
-                      </div>
+                      {image && (
+                        <img
+                          className="ins-c-application-info__logo"
+                          aria-hidden
+                          src={image}
+                          alt={`${title} logo`}
+                        />
+                      )}
+                      {Icon && (
+                        <Icon
+                          className="ins-c-application-info__logo ins-c-icon__active"
+                          aria-hidden
+                          size="xl"
+                          alt={`${title} logo`}
+                          {...iconProps}
+                        />
+                      )}
                     </StackItem>
-                  )}
-                  {isDevPreview && (
                     <StackItem>
-                      <div className="ins-m-dev-preview">
-                        <CodeIcon size="sm" /> Developer preview
-                      </div>
+                      <Title headingLevel="h2">{title}</Title>
                     </StackItem>
-                  )}
-                  {isEarlyAccess && (
-                    <StackItem>
-                      <Tooltip
-                        position={TooltipPosition.bottom}
-                        content={<span>Available to limited customers</span>}
-                      >
-                        <div className="ins-m-early-preview">
-                          <OutlinedEyeIcon size="sm" /> Early access preview
+                  </Stack>
+                </CardHeader>
+                <CardBody>
+                  <Stack hasGutter className="ins-c-application-info__content">
+                    {isPreview && (
+                      <StackItem>
+                        <div className="ins-m-tech-preview">
+                          <BinocularsIcon size="sm" /> Tech preview
                         </div>
-                      </Tooltip>
-                    </StackItem>
-                  )}
-                  <StackItem>
-                    <span className="ins-m-gray">{body}</span>
-                  </StackItem>
-                  <StackItem className="ins-c-application-info__content-applist">
-                    {apps &&
-                      Object.entries(apps).map(([appName, appPath]) => (
-                        <Button
-                          component="a"
-                          isDisabled={
-                            isUnderMaintenance ||
-                            checkIfUnderMaintenance(
-                              appName,
-                              isUnderMaintenanceApps
-                            )
-                          }
-                          isInline
-                          variant="link"
-                          key={appName}
-                          href={`${isBeta()}${url}${appPath}`}
+                      </StackItem>
+                    )}
+                    {isDevPreview && (
+                      <StackItem>
+                        <div className="ins-m-dev-preview">
+                          <CodeIcon size="sm" /> Developer preview
+                        </div>
+                      </StackItem>
+                    )}
+                    {isEarlyAccess && (
+                      <StackItem>
+                        <Tooltip
+                          position={TooltipPosition.bottom}
+                          content={<span>Available to limited customers</span>}
                         >
-                          {appName}
-                        </Button>
-                      ))}
-                  </StackItem>
-                </Stack>
-              </CardBody>
-              <CardFooter>
-                <Button
-                  component="a"
-                  isInline
-                  isDisabled={isUnderMaintenance}
-                  variant="link"
-                  className={`ins-c-application-info__open ins-c-application-info__open-${url}`}
-                  href={
-                    baseApp
-                      ? `${isBeta()}${url}${baseApp}`
-                      : `${isBeta()}${url}`
-                  }
-                >
-                  <span> Open </span>
-                  <ArrowRightIcon size="sm" />
-                </Button>
-              </CardFooter>
-            </Card>
-          </GridItem>
-        ))}
+                          <div className="ins-m-early-preview">
+                            <OutlinedEyeIcon size="sm" /> Early access preview
+                          </div>
+                        </Tooltip>
+                      </StackItem>
+                    )}
+                    <StackItem>
+                      <span className="ins-m-gray">{body}</span>
+                    </StackItem>
+                    <StackItem className="ins-c-application-info__content-applist">
+                      {apps &&
+                        Object.entries(apps).map(([appName, appPath]) => (
+                          <Button
+                            component="a"
+                            isDisabled={
+                              isUnderMaintenance ||
+                              checkIfUnderMaintenance(
+                                appName,
+                                isUnderMaintenanceApps
+                              )
+                            }
+                            isInline
+                            variant="link"
+                            key={appName}
+                            href={`${isBeta()}${url}${appPath}`}
+                          >
+                            {appName}
+                          </Button>
+                        ))}
+                    </StackItem>
+                  </Stack>
+                </CardBody>
+                <CardFooter>
+                  <Button
+                    component="a"
+                    isInline
+                    isDisabled={isUnderMaintenance}
+                    variant="link"
+                    className={`ins-c-application-info__open ins-c-application-info__open-${url}`}
+                    href={
+                      baseApp
+                        ? `${isBeta()}${url}${baseApp}`
+                        : `${isBeta()}${url}`
+                    }
+                  >
+                    <span> Open </span>
+                    <ArrowRightIcon size="sm" />
+                  </Button>
+                </CardFooter>
+              </Card>
+            </GridItem>
+          ))}
       </Grid>
     </PageSection>
   );
--- src/layout/Marketing.js
+++ src/layout/Marketing.js
@@ -24,55 +24,60 @@ const Marketing = () => (
       landing-page-type="unauthenticated"
     >
       <Grid sm={12} md={6} xl={6} xl2={4} gutter="sm">
-        {technologies.map(
-          (
-            { marketingImage, title, marketingUrls, marketingText, id },
-            key
-          ) => (
-            <Card
-              className="ins-c-application-info pf-m-card-link"
-              application-id={id}
-              key={key}
-            >
-              <CardHeader>
-                <Stack gutter="sm">
-                  <StackItem className="ins-c-application-logo">
-                    {marketingImage && (
-                      <img
-                        className="ins-c-application-info__logo"
-                        aria-hidden
-                        src={marketingImage}
-                        alt={`${title} logo`}
-                      />
-                    )}
-                  </StackItem>
-                </Stack>
-              </CardHeader>
-              <CardBody>
-                <Stack>
-                  <StackItem>
-                    <span className="ins-m-gray">{marketingText}</span>
-                  </StackItem>
-                </Stack>
-              </CardBody>
-              <CardFooter className="pf-c-card__card-links">
-                <a href={marketingUrls.learnMore} aria-label={`Go to ${title}`}>
-                  Learn more
-                  <ArrowRightIcon size="sm" />
-                </a>
-                {marketingUrls.tryIt && (
+        {technologies
+          .filter(({ marketing }) => marketing)
+          .map(
+            (
+              { marketingImage, title, marketingUrls, marketingText, id },
+              key
+            ) => (
+              <Card
+                className="ins-c-application-info pf-m-card-link"
+                application-id={id}
+                key={key}
+              >
+                <CardHeader>
+                  <Stack gutter="sm">
+                    <StackItem className="ins-c-application-logo">
+                      {marketingImage && (
+                        <img
+                          className="ins-c-application-info__logo"
+                          aria-hidden
+                          src={marketingImage}
+                          alt={`${title} logo`}
+                        />
+                      )}
+                    </StackItem>
+                  </Stack>
+                </CardHeader>
+                <CardBody>
+                  <Stack>
+                    <StackItem>
+                      <span className="ins-m-gray">{marketingText}</span>
+                    </StackItem>
+                  </Stack>
+                </CardBody>
+                <CardFooter className="pf-c-card__card-links">
                   <a
-                    href={marketingUrls.tryIt}
-                    aria-label="Request an evaluation"
+                    href={marketingUrls.learnMore}
+                    aria-label={`Go to ${title}`}
                   >
-                    Try it
+                    Learn more
                     <ArrowRightIcon size="sm" />
                   </a>
-                )}
-              </CardFooter>
-            </Card>
-          )
-        )}
+                  {marketingUrls.tryIt && (
+                    <a
+                      href={marketingUrls.tryIt}
+                      aria-label="Request an evaluation"
+                    >
+                      Try it
+                      <ArrowRightIcon size="sm" />
+                    </a>
+                  )}
+                </CardFooter>
+              </Card>
+            )
+          )}
       </Grid>
     </PageSection>
   </React.Fragment>
