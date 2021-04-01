253a7ef922fe49d55235813bb090fe63d5d05e0f
rvsia <r.vsia@seznam.cz>
2021-04-01 10:27:43 +0200

    Check that permission are an array

--- src/utils/allPermissions.js
+++ src/utils/allPermissions.js
@@ -4,9 +4,11 @@ export const loadPermissions = async (retries = 5) => {
   try {
     if (retries > 0) {
       const userPermissions = await window.insights.chrome.getUserPermissions();
-      permissions = userPermissions.map(({ permission }) => permission);
+      if (Array.isArray(userPermissions)) {
+        permissions = userPermissions.map(({ permission }) => permission);
+      }
     }
-  } catch {
+  } catch (e) {
     await loadPermissions(retries - 1);
   }
 };
