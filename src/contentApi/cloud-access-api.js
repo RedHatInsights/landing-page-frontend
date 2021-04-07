4c98d298f4b0c0ca314e589e43715d45958bad2e
Martin Marosi <marvusm.mmi@gmail.com>
2021-04-07 10:59:07 +0200

    Add cloud access content.

---
+++ src/contentApi/cloud-access-api.js
@@ -0,0 +1,56 @@
+const noSourcesProcessor = (response) => {
+  const count = response?.meta?.count;
+  if (count > 0) {
+    return {
+      ...response,
+      shape: {
+        icon: 'unknown',
+        title: 'Manage your Red Hat products in the cloud',
+        description:
+          'Connect to additional accounts or public cloud providers.',
+        link: {
+          href:
+            './settings/sources?sort_by[]=created_at:desc&limit=50&offset=0&activeVendor=Cloud',
+          title: 'Connect with Sources',
+        },
+      },
+    };
+  }
+
+  return response;
+};
+
+const CLOUD_ACCESS_CONFIGURE = [
+  {
+    key: 'connect-sources',
+    url:
+      '/api/sources/v3.0/sources?filter[source_type][vendor][eq][]=Amazon&filter[source_type][vendor][eq][]=Azure&filter[source_type][vendor][eq][]=Google',
+    responseProcessor: noSourcesProcessor,
+    shape: {
+      icon: 'unknown',
+      title: 'Connect to your public clouds',
+      description:
+        'Register a provider to manage your Red Hat products in the cloud.',
+      link: {
+        href:
+          './settings/sources?sort_by[]=created_at:desc&limit=50&offset=0&activeVendor=Cloud',
+        title: 'Connect with Sources',
+      },
+    },
+    permissions: [
+      {
+        method: 'isOrgAdmin',
+      },
+    ],
+  },
+];
+
+export const getCloudAccessDataSchema = () => ({
+  firstPanel: [],
+  secondPanel: [],
+  configTryLearn: {
+    configure: CLOUD_ACCESS_CONFIGURE,
+    try: [],
+    learn: [],
+  },
+});
--- src/contentApi/get-apps-data.js
+++ src/contentApi/get-apps-data.js
@@ -1,9 +1,11 @@
 import { getAnsibleDataSchema } from './ansible-api';
+import { getCloudAccessDataSchema } from './cloud-access-api';
 import { getCostDataSchema } from './cost-api';
 import { createRhelSchema } from './rhel';
 
 const getAppsData = () => {
   const data = [
+    getCloudAccessDataSchema(),
     createRhelSchema(),
     getAnsibleDataSchema(),
     getCostDataSchema(),
