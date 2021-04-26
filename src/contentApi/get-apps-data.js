0bb355b05b96bf01fbe91c766b860372e7289978
Martin Marosi <marvusm.mmi@gmail.com>
2021-04-26 08:43:47 +0200

    Enable managed services content in all environments.

--- src/contentApi/get-apps-data.js
+++ src/contentApi/get-apps-data.js
@@ -9,17 +9,12 @@ import { getManagedServicesDataSchema } from './managed-services-api';
 const getAppsData = () => {
   const data = [
     getPriorityDataSchema(),
+    getManagedServicesDataSchema(),
     createRhelSchema(),
     getAnsibleDataSchema(),
     getFifiDataSchema(),
     getCostDataSchema(),
   ];
-  if (
-    window.insights.chrome.getEnvironment() === 'stage' &&
-    window.insights.chrome.isBeta()
-  ) {
-    data.push(getManagedServicesDataSchema());
-  }
   return data;
 };
 
