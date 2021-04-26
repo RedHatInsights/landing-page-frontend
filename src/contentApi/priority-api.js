960c279e6b80939b5de44ff0fb3265b8ef7febf8
Ryan Long <rlong@redhat.com>
2021-04-26 14:55:59 -0400

    lint

--- src/contentApi/priority-api.js
+++ src/contentApi/priority-api.js
@@ -66,7 +66,8 @@ export const getPriorityDataSchema = () => ({
       {
         shape: {
           title: 'Add public cloud sources to better track your finances',
-          description: 'If your OpenShift cluster is running on a public cloud, add that cloud account to Sources for better analysis.',
+          description:
+            'If your OpenShift cluster is running on a public cloud, add that cloud account to Sources for better analysis.',
           link: {
             title: 'Connect',
             href: './settings/sources',
