b0e1bd1474a30ad7dee19ccfb53efff5265c59ce
Ryan Long <rlong@redhat.com>
2021-04-28 16:50:47 -0400

    remove headers for now

--- src/contentApi/request-processor.js
+++ src/contentApi/request-processor.js
@@ -21,13 +21,6 @@ const enhancedFunctions = {
 
 const ALLOWED_API_METHODS = ['get', 'post'];
 
-// the Authorization header is needed for api.openshift, 3scale will set this for all CRC hosted APIs, but not openshift :(
-const headers = {
-  Accept: 'application/json',
-  Authorization: `Bearer ${Cookies.get('cs_jwt')}`,
-  'Content-Type': 'application/json',
-};
-
 export const processRequest = async ({
   method = 'get',
   args = [],
@@ -56,7 +49,6 @@ export const processRequest = async ({
        */
       response = await fetch(url, {
         method,
-        headers,
         data: JSON.stringify(args[0]),
       }).then((d) => d.json());
     }
