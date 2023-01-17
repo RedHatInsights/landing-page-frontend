1c04fac3c4340a8303b1c62652b4e21b78fd8bd7
Brandon Tweed <btweed@redhat.com>
2023-01-17 14:26:54 -0600

    Update pr_check.sh

--- pr_check.sh
+++ pr_check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 export COMPONENT="landing-page-frontend"
 export IMAGE="quay.io/cloudservices/$COMPONENT"
 export WORKSPACE=${WORKSPACE:-$APP_ROOT}  # if running in jenkins, use the build's workspace
@@ -15,6 +16,37 @@ set -ex
 # source is preferred to | bash -s in this case to avoid a subshell
 source <(curl -sSL $COMMON_BUILDER/src/frontend-build.sh)
 BUILD_RESULTS=$?
+
+echo "Taking a short nap"
+sleep 120
+
+# Ensure that we deploy the right component for testing
+export APP_NAME=rbac
+export COMPONENT="rbac"
+export COMPONENT_NAME="rbac"
+
+# Install bonfire
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh 
+
+source .cicd_bootstrap.sh
+
+set -x
+# Deploy to an ephemeral namespace for testing
+export GIT_COMMIT=master
+source $CICD_ROOT/deploy_ephemeral_env.sh
+
+set +x
+# Run some tests with ClowdJobInvocation
+IQE_PLUGINS="platform_ui"
+IQE_MARKER_EXPRESSION="smoke"
+IQE_FILTER_EXPRESSION=""
+IQE_ENV="ephemeral"
+IQE_SELENIUM="true"
+IQE_CJI_TIMEOUT="30m"
+DEPLOY_TIMEOUT="900"  # 15min
+DEPLOY_FRONTENDS="true"
+source $CICD_ROOT/cji_smoke_test.sh
 
 # Stubbed out for now
 mkdir -p $WORKSPACE/artifacts
