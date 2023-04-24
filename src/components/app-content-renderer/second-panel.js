c3b3a73d37b640a2d48d4c4c93ac86224a0ba475
Martin Maroši <marvusm.mmi@gmail.com>
2023-04-24 16:30:42 +0200

    Update ACS prod stable link.

--- src/components/app-content-renderer/second-panel.js
+++ src/components/app-content-renderer/second-panel.js
@@ -19,8 +19,14 @@ import { Link } from 'react-router-dom';
 import ArrowRightIcon from '@patternfly/react-icons/dist/js/icons/arrow-right-icon';
 import StarIcon from '@patternfly/react-icons/dist/js/icons/star-icon';
 import RecentlyVisited from '../recently-visited/recently-visited';
+import { useChrome } from '@redhat-cloud-services/frontend-components/useChrome';
 
 const SecondPanel = () => {
+  const { isProd, isBeta } = useChrome();
+  const redirectToBeta = isProd() && !isBeta();
+  const ACSLink = redirectToBeta
+    ? ({ to, children }) => <a href={`/beta${to}`}>{children}</a>
+    : Link;
   return (
     <Sidebar className="land-c-sidebar pf-u-background-color-100">
       <SidebarContent className="pf-m-no-background pf-u-px-xl pf-u-pt-md pf-u-pb-2xl-on-md">
@@ -201,12 +207,12 @@ const SecondPanel = () => {
                   applications and Kubernetes.
                 </Text>
                 <Text component={TextVariants.p}>
-                  <Link to="/application-services/acs/instances">
+                  <ACSLink to="/application-services/acs/instances">
                     Get Started Now
                     <Icon className="pf-u-ml-sm" isInline>
                       <ArrowRightIcon />
                     </Icon>
-                  </Link>
+                  </ACSLink>
                 </Text>
               </TextContent>
             </CardBody>
