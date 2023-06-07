7f93029f9ec826b540896eee0e1142798fca919e
Brandon Tweed <btweed@redhat.com>
2023-06-07 05:35:02 -0500

    RHCLOUD-23727: Run IQE UI tests as part of PR check (#453)
    * Update pr_check.sh
    
    * w
    
    * potato
    
    * w
    
    * w
    
    * Fix image tag for CJI
    
    * w
    
    * try again
    
    * w
    
    * w
    
    * Exclude progressive profile tests
    
    * Exclude apidocs tests from pr check
    
    * wip
    
    * wip

--- pr_check.sh
+++ pr_check.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 export COMPONENT="landing-page-frontend"
 export IMAGE="quay.io/cloudservices/$COMPONENT"
 export WORKSPACE=${WORKSPACE:-$APP_ROOT}  # if running in jenkins, use the build's workspace
@@ -15,6 +16,43 @@ set -ex
 # source is preferred to | bash -s in this case to avoid a subshell
 source <(curl -sSL $COMMON_BUILDER/src/frontend-build.sh)
 BUILD_RESULTS=$?
+
+# Ensure that we deploy the right component for testing
+export APP_NAME=rbac
+export COMPONENT="rbac"
+export COMPONENT_NAME="rbac"
+
+# Install bonfire
+CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
+curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh 
+
+source .cicd_bootstrap.sh
+
+echo "Taking a short nap"
+sleep 60
+
+
+set -x
+# Deploy to an ephemeral namespace for testing
+export IMAGE="quay.io/cloudservices/rbac"
+export GIT_COMMIT=master
+export IMAGE_TAG=latest
+export DEPLOY_FRONTENDS=true
+source $CICD_ROOT/deploy_ephemeral_env.sh
+
+# Run some tests with ClowdJobInvocation
+export IQE_IMAGE_TAG="platform-ui"
+IQE_PLUGINS="platform_ui"
+IQE_MARKER_EXPRESSION="smoke"
+# Exclude progressive profile tests
+# Exclude APIdocs tests
+IQE_FILTER_EXPRESSION="not (test_progressive or test_apidocs)"
+IQE_ENV="ephemeral"
+IQE_SELENIUM="true"
+IQE_CJI_TIMEOUT="30m"
+DEPLOY_TIMEOUT="900"  # 15min
+DEPLOY_FRONTENDS="true"
+source $CICD_ROOT/cji_smoke_test.sh
 
 # Stubbed out for now
 mkdir -p $WORKSPACE/artifacts
