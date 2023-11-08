47b6cb4315ce92fbc97c147b730ac869c78a7cd8
Josejulio Martínez <josejulio@users.noreply.github.com>
2023-11-08 09:01:40 -0700

    - Disable for prod - Lint fix

--- src/components/app-content-renderer/virtual-assistant.js
+++ src/components/app-content-renderer/virtual-assistant.js
@@ -3,25 +3,28 @@ import React from 'react';
 import { ScalprumComponent } from '@scalprum/react-core';
 
 import './virtual-assistant.scss';
-import useChrome from "@redhat-cloud-services/frontend-components/useChrome";
+import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
 
 function VirtualAssistant() {
-    const showVirtualAssistant = useFlag('platform.landing-page.virtual-assistant');
-    const chrome = useChrome();
+  const showVirtualAssistant = useFlag(
+    'platform.landing-page.virtual-assistant'
+  );
+  const chrome = useChrome();
 
-    if (!chrome.isBeta || !showVirtualAssistant) {
-        return null;
-    }
+  // Disable it for prod, any stable environment or if the feature flag is off
+  if (chrome.isProd() || !chrome.isBeta || !showVirtualAssistant) {
+    return null;
+  }
 
-    return (
-        <div className="virtualAssistant astro__landing-page">
-            <ScalprumComponent
-                scope="virtualAssistant"
-                module="./AstroVirtualAssistant"
-                fallback={null}
-            />
-        </div>
-    );
+  return (
+    <div className="virtualAssistant astro__landing-page">
+      <ScalprumComponent
+        scope="virtualAssistant"
+        module="./AstroVirtualAssistant"
+        fallback={null}
+      />
+    </div>
+  );
 }
 
 export default VirtualAssistant;
--- src/routes/Landing.js
+++ src/routes/Landing.js
@@ -4,7 +4,7 @@ import '../components/app-content-renderer/styles/panels.scss';
 
 import FirstPanel from '../components/app-content-renderer/first-panel';
 import SecondPanel from '../components/app-content-renderer/second-panel';
-import VirtualAssistant from "../components/app-content-renderer/virtual-assistant";
+import VirtualAssistant from '../components/app-content-renderer/virtual-assistant';
 
 const Landing = () => {
   return (
