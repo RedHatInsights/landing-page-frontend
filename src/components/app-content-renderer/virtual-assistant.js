47e23255e533388023614683fbae3d8ab6338162
Josejulio Martínez <josejulio@users.noreply.github.com>
2023-11-08 08:42:26 -0700

    RHCLOUD-29161 Deploy virtual-assistant on the landing page  - Behind feature flag "platform.landing-page.virtual-assistant"  - Only on beta/preview

---
+++ src/components/app-content-renderer/virtual-assistant.js
@@ -0,0 +1,27 @@
+import { useFlag } from '@unleash/proxy-client-react';
+import React from 'react';
+import { ScalprumComponent } from '@scalprum/react-core';
+
+import './virtual-assistant.scss';
+import useChrome from "@redhat-cloud-services/frontend-components/useChrome";
+
+function VirtualAssistant() {
+    const showVirtualAssistant = useFlag('platform.landing-page.virtual-assistant');
+    const chrome = useChrome();
+
+    if (!chrome.isBeta || !showVirtualAssistant) {
+        return null;
+    }
+
+    return (
+        <div className="virtualAssistant astro__landing-page">
+            <ScalprumComponent
+                scope="virtualAssistant"
+                module="./AstroVirtualAssistant"
+                fallback={null}
+            />
+        </div>
+    );
+}
+
+export default VirtualAssistant;
--- src/routes/Landing.js
+++ src/routes/Landing.js
@@ -4,11 +4,13 @@ import '../components/app-content-renderer/styles/panels.scss';
 
 import FirstPanel from '../components/app-content-renderer/first-panel';
 import SecondPanel from '../components/app-content-renderer/second-panel';
+import VirtualAssistant from "../components/app-content-renderer/virtual-assistant";
 
 const Landing = () => {
   return (
     <div className="land-c-page-content pf-u-display-flex pf-u-flex-direction-column">
       <Fragment>
+        <VirtualAssistant />
         <FirstPanel />
         <SecondPanel />
       </Fragment>
