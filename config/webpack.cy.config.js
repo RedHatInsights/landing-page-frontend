ce5bcbbd1c0a91d07aa5641aa42a5118ff5673ee
Zein Sleiman <zsleiman@redhat.com>
2024-01-15 15:30:58 -0600

    add cypress config

---
+++ config/webpack.cy.config.js
@@ -0,0 +1,144 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const {
+  createJoinFunction,
+  createJoinImplementation,
+  asGenerator,
+  defaultJoinGenerator,
+} = require('resolve-url-loader');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+const searchIgnoredStyles = require('@redhat-cloud-services/frontend-components-config-utilities/search-ignored-styles');
+
+// call default generator then pair different variations of uri with each base
+const PFGenerator = asGenerator((item, ...rest) => {
+  const defaultTuples = [...defaultJoinGenerator(item, ...rest)];
+  if (item.uri.includes('./assets')) {
+    return defaultTuples.map(([base]) => {
+      if (base.includes('pf-4-styles')) {
+        return [
+          base,
+          path.relative(
+            base,
+            path.resolve(__dirname, '../node_modules/pf-4-styles', item.uri)
+          ),
+        ];
+      }
+      if (base.includes('@patternfly/patternfly')) {
+        return [
+          base,
+          path.relative(
+            base,
+            path.resolve(
+              __dirname,
+              '../node_modules/@patternfly/patternfly',
+              item.uri
+            )
+          ),
+        ];
+      }
+    });
+  }
+  return defaultTuples;
+});
+
+/** @type { import("webpack").Configuration } */
+const JSConfig = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'swc-loader',
+          options: {
+            jsc: {
+              experimental: {
+                plugins: [
+                  [
+                    'swc-plugin-coverage-instrument',
+                    {
+                      compact: false,
+                    },
+                  ],
+                ],
+              },
+              parser: {
+                syntax: 'typescript',
+                tsx: true,
+              },
+            },
+          },
+        },
+      },
+      {
+        test: /\.s?[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'resolve-url-loader',
+            options: {
+              join: createJoinFunction(
+                'myJoinFn',
+                createJoinImplementation(PFGenerator)
+              ),
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|svg|png|gif|ico|eot|ttf|woff2?)(\?v=\d+\.\d+\.\d+)?$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    alias: {
+      ...searchIgnoredStyles(path.resolve(__dirname, '../')),
+    },
+  },
+  output: {
+    filename: 'bundle.js',
+    hashFunction: 'xxhash64',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  cache: {
+    type: 'filesystem',
+    buildDependencies: {
+      config: [__filename],
+    },
+    cacheDirectory: path.resolve(__dirname, '../.cypress-cache'),
+  },
+  stats: {
+    errorDetails: true,
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new ModuleFederationPlugin({
+      name: 'chrome',
+      filename: 'chrome.js',
+      shared: [
+        { react: { singleton: true, eager: true } },
+        { 'react-dom': { singleton: true, eager: true } },
+        { 'react-router-dom': { singleton: true } },
+        { 'react-redux': {} },
+        { '@patternfly/react-core': {} },
+        { '@patternfly/quickstarts': { singleton: true } },
+        { '@scalprum/react-core': { singleton: true } },
+        { '@unleash/proxy-client-react': { singleton: true } },
+      ],
+    }),
+  ],
+};
+
+module.exports = JSConfig;
---
+++ cypress.config.ts
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { defineConfig } from 'cypress';
+const {
+  addMatchImageSnapshotPlugin,
+} = require('@simonsmith/cypress-image-snapshot/plugin');
+
+export default defineConfig({
+  component: {
+    specPattern: 'cypress/component/**/*.cy.{js,jsx,ts,tsx}',
+    excludeSpecPattern: ['/snapshots/*', '/image_snapshots/*', '/src/*'],
+    setupNodeEvents(on, config) {
+      addMatchImageSnapshotPlugin(on, config);
+      on('before:browser:launch', (browser, launchOptions) => {
+        if (browser.name === 'chrome' && browser.isHeadless) {
+          launchOptions.args.push('--window-size=1280,720');
+
+          // force screen to be non-retina
+          launchOptions.args.push('--force-device-scale-factor=1');
+        }
+
+        if (browser.name === 'electron' && browser.isHeadless) {
+          // fullPage screenshot size is 1280x720
+          launchOptions.preferences.width = 1280;
+          launchOptions.preferences.height = 720;
+        }
+      });
+      require('@cypress/code-coverage/task')(on, config);
+      return config;
+    },
+    video: false,
+    devServer: {
+      framework: 'react',
+      bundler: 'webpack',
+      webpackConfig: require('./config/webpack.cy.config.js'),
+    },
+  },
+  e2e: {
+    blockHosts: ['consent.trustarc.com'],
+    baseUrl: 'https://stage.foo.redhat.com:1337/beta',
+    env: {
+      E2E_USER: process.env.E2E_USER,
+      E2E_PASSWORD: process.env.E2E_PASSWORD,
+    },
+    screenshotOnRunFailure: false,
+    // required for the SSO redirect
+    chromeWebSecurity: false,
+    video: false,
+    setupNodeEvents(on, config) {
+      require('cypress-localstorage-commands/plugin')(on, config);
+      return config;
+      // implement node event listeners here
+    },
+  },
+});
