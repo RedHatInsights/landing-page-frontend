e57596c4a52d5f6009ee45e433ff3e4b145c8b76
Bryan Florkiewicz <bflorkie@redhat.com>
2024-03-15 10:53:18 -0400

    RHCLOUD-31204: Add widget layout fed mod dependency (#543)
    * Add widget layout fed mod dependency
    
    * Update src/routes/Landing.tsx
    
    * Update fec.config.js
    
    * Update fec.config.js
    
    * Update fec.config.js
    
    ---------
    
    Co-authored-by: Karel Hala <ch4rlien@gmail.com>

--- fec.config.js
+++ fec.config.js
@@ -8,11 +8,35 @@ module.exports = {
   proxyVerbose: true,
   hotReload: true,
   plugins: [],
+  routes: {
+    ...(process.env.CONFIG_PORT && {
+      '/api/chrome-service/v1/static': {
+        host: `http://localhost:${process.env.CONFIG_PORT}`,
+      },
+      '/api/chrome-service/v1/dashboard-templates': {
+        host: `http://localhost:${process.env.CONFIG_PORT}`,
+      },
+    }),
+    ...(process.env.LOCAL_WIDGET_LAYOUT && {
+      '/apps/widget-layout': {
+        host: `http://localhost:8003`,
+      },
+      '/beta/apps/widget-layout': {
+        host: `http://localhost:8003`,
+      },
+    }),
+  },
   moduleFederation: {
     exposes: {
       './RootApp': path.resolve(__dirname, './src/moduleEntries/AppEntry.tsx'),
     },
     exclude: ['react-router-dom'],
-    shared: [{ 'react-router-dom': { singleton: true, version: '*' } }],
+    shared: [
+      { 'react-router-dom': { singleton: true, version: '*' } },
+      { frontendStarterApp: { singleton: true, version: '*' } },
+      { '@scalprum/react-core': { singleton: true, version: '*' } },
+      { '@patternfly/react-core': { singleton: true, version: '*' } },
+      { 'react-redux': { singleton: true, version: '*' } },
+    ],
   },
 };
