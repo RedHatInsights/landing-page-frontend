e694ed695d5f8aa79a219e8137c4658528f9b5b0
alex <86322239+aferd@users.noreply.github.com>
2024-03-21 15:03:48 -0400

    RHCLOUD-30990 - Create simple service widgets (#548)
    * add simple widgets
    
    * add to fec config

---
+++ src/components/widgets/openshift-widget.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SimpleServiceWidget } from './simple-service-widget';
+
+export const OpenShiftWidget: React.FunctionComponent = () => {
+  return (
+    <>
+      <SimpleServiceWidget
+        id={1}
+        body="Build, run, and scale container-based applications - now with developer tools, CI/CD, and release management."
+        linkTitle="OpenShift"
+        url="/openshift"
+      />
+    </>
+  );
+};
