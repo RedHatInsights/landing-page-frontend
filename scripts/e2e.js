741cb47209586c10d3af77cfdae62f69fc08e60a
Zein Sleiman <zsleiman@redhat.com>
2024-05-06 15:43:21 -0500

    fix e2e script

--- scripts/e2e.js
+++ scripts/e2e.js
@@ -26,7 +26,7 @@ async function runTests() {
   );
   await waitOn(options);
   execSync(
-    `NO_COLOR=1 E2E_USER=${process.env.CHROME_ACCOUNT} E2E_PASSWORD=${process.env.CHROME_PASSWORD} npm run cypress run`,
+    `NO_COLOR=1 E2E_USER=${process.env.CHROME_ACCOUNT} E2E_PASSWORD=${process.env.CHROME_PASSWORD} npm run cypress:run:e2e`,
     {
       encoding: 'utf-8',
       stdio: 'inherit',
