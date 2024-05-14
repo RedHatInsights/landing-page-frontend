5e9401200ed50455905bd3c81c14d1b2bf4b7d8f
Brandon Tweed <btweed@redhat.com>
2024-05-14 14:59:35 -0500

    RHCLOUD-32154: Cypress test to remove all widgets

---
+++ cypress/e2e/widgets-landing.cy.ts
@@ -0,0 +1,47 @@
+function resetToDefaultLayout() {
+  cy.get('button')
+    .contains('Reset to default')
+    .click()
+    .get('#warning-modal-check')
+    .click()
+    .get("button[data-ouia-component-id='primary-confirm-button']")
+    .click();
+}
+
+describe('Widget Landing Page', () => {
+  it('closes all the widgets', () => {
+    cy.login();
+    cy.visit('/');
+
+    // Reset layout to the default
+    resetToDefaultLayout();
+    
+    // Ensure that widgets are open and displayed (Number of items in grid expected to be numDefaultWidgets)
+    const numDefaultWidgets = 9;
+    cy.get(
+      '.widgetLayout > section > div > .react-grid-layout > .react-grid-item'
+    )
+      .its('length')
+      .should('be.eq', numDefaultWidgets)
+      .wait(1500);
+
+    const cardActionsSelector =
+      '.widgetLayout > section > div > .react-grid-layout > .react-grid-item > div > .pf-v5-c-card__header > .pf-v5-c-card__actions > div > button';
+    
+    // Close all the widgets
+    for (let i = 0; i < numDefaultWidgets; i++) {
+      cy.get(cardActionsSelector)
+        .first()
+        .click()
+        .get('body > div.pf-v5-c-menu > div > ul > li:nth-child(4) > button')
+        .click()
+        .wait(1500);
+    }
+
+    // Confirm that the "empty" message is displayed
+    cy.get('h2').contains('No dashboard content');
+
+    // Reset to default layout
+    resetToDefaultLayout();
+  });
+});
