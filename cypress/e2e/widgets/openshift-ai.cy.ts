afd12d8ee19452baad071a2dbb69dafda650f89f
Karel Hala <khala@redhat.com>
2024-05-28 16:12:22 +0200

    Add e2e tests for openshift AI widget

---
+++ cypress/e2e/widgets/openshift-ai.cy.ts
@@ -0,0 +1,48 @@
+describe('Openshift AI widget', () => {
+  beforeEach(() => {
+    cy.login();
+    cy.visit('/');
+
+    cy.intercept(
+      'GET',
+      '**/api/chrome-service/v1/dashboard-templates?dashboard=landingPage'
+    ).as('resetLayout');
+
+    cy.intercept('PATCH', '**/api/chrome-service/v1/dashboard-templates/*').as(
+      'patchLayout'
+    );
+
+    cy.wait('@resetLayout').its('response.statusCode').should('eq', 200);
+  });
+
+  it('should appear on default layout', () => {
+    cy.get(`[data-ouia-component-id="landing-openshiftAi-widget"]`).should(
+      'exist'
+    );
+  });
+
+  it('should have correct link', () => {
+    cy.get(`[data-ouia-component-id="landing-openshiftAi-widget"] a`)
+      .should('have.attr', 'href')
+      .and(
+        'include',
+        'https://www.redhat.com/en/technologies/cloud-computing/openshift/openshift-ai/trial'
+      );
+  });
+
+  it('should be removed if clicked on remove', () => {
+    cy.get(
+      `[data-ouia-component-id="landing-openshiftAi-widget"] button.pf-v5-c-menu-toggle`
+    ).click();
+    cy.contains('.pf-v5-c-menu__item-text', 'Remove').click();
+    cy.wait('@patchLayout').then(({ response }) => {
+      expect(response?.statusCode).to.eq(200);
+      const firstMove = response?.body?.data;
+      expect(firstMove).to.not.be.null;
+
+      cy.get(`[data-ouia-component-id="landing-openshiftAi-widget"]`).should(
+        'not.exist'
+      );
+    });
+  });
+});
