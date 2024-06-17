3771556051005c760ea3d360d676970525a99386
Martin Marosi <marvusm.mmi@gmail.com>
2024-06-17 13:58:45 +0200

    Add widget layout CSS scope.

--- src/routes/Landing.tsx
+++ src/routes/Landing.tsx
@@ -5,35 +5,35 @@ import '../components/app-content-renderer/styles/panels.scss';
 
 import FirstPanel from '../components/app-content-renderer/first-panel';
 import SecondPanel from '../components/app-content-renderer/second-panel';
-import { useLoadModule } from '@scalprum/react-core';
+import { ScalprumComponent } from '@scalprum/react-core';
+import classNames from 'classnames';
 
 import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
 
-const getWidgetLayoutLandingPage = () => {
-  const scope = 'widgetLayout';
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const [{ default: WidgetLayout }] = useLoadModule(
-    { scope, module: './WidgetLayout' },
-    {}
-  );
-  if (!WidgetLayout) {
-    return <></>;
-  }
-  return <WidgetLayout isLayoutLocked={false} layoutType={'landingPage'} />;
-};
-
 const Landing = () => {
+  const scope = 'widgetLayout';
   const { isBeta } = useChrome();
   const widgetLayoutLandingPageEnabled =
     (isBeta() && useFlag('platform.landing-page.widgetization')) ||
     (!isBeta() && useFlag('platform.landing-page.widgetization-stable'));
   return (
-    <div className="land-c-page-content pf-v5-u-background-color-200 pf-v5-u-display-flex pf-v5-u-flex-direction-column">
+    <div
+      className={classNames(
+        'land-c-page-content pf-v5-u-background-color-200 pf-v5-u-display-flex pf-v5-u-flex-direction-column',
+        scope
+      )}
+    >
       <Fragment>
         {widgetLayoutLandingPageEnabled ? null : <FirstPanel />}
         {widgetLayoutLandingPageEnabled ? null : <SecondPanel />}
-        {widgetLayoutLandingPageEnabled ? getWidgetLayoutLandingPage() : null}
+        {widgetLayoutLandingPageEnabled ? (
+          <ScalprumComponent
+            fallback={null}
+            LoadingComponent={() => <></>}
+            scope={scope}
+            module="./WidgetLayout"
+          />
+        ) : null}
       </Fragment>
     </div>
   );
