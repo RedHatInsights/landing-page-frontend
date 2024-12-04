c9c1afef3567028452d4038096e85a8262b6697b
epwinchell <1287144+epwinchell@users.noreply.github.com>
2024-12-04 12:32:23 -0500

    new 'explore capabilities' widget (#638)
    * new 'explore capabilities' widget
    
    * lint fix

--- src/components/widgets/explore-capabilities.tsx
+++ src/components/widgets/explore-capabilities.tsx
@@ -1,193 +1,92 @@
 import React from 'react';
-import { Button } from '@patternfly/react-core/dist/dynamic/components/Button';
 import {
   Card,
   CardBody,
 } from '@patternfly/react-core/dist/dynamic/components/Card';
-import { Drawer } from '@patternfly/react-core/dist/dynamic/components/Drawer';
-import { DrawerContent } from '@patternfly/react-core/dist/dynamic/components/Drawer';
-import { DrawerContentBody } from '@patternfly/react-core/dist/dynamic/components/Drawer';
-import { DrawerPanelBody } from '@patternfly/react-core/dist/dynamic/components/Drawer';
-import { DrawerPanelContent } from '@patternfly/react-core/dist/dynamic/components/Drawer';
-import { SimpleList } from '@patternfly/react-core/dist/dynamic/components/SimpleList';
-import { SimpleListItem } from '@patternfly/react-core/dist/dynamic/components/SimpleList';
-import './explore-capabilities.scss';
+import { Gallery } from '@patternfly/react-core/dist/dynamic/layouts/Gallery';
 import {
   Text,
   TextContent,
 } from '@patternfly/react-core/dist/dynamic/components/Text';
+import { Link } from 'react-router-dom';
+
+import './explore-capabilities.scss';
 
 const ExploreCapabilities: React.FunctionComponent = () => {
-  const [activeItem, setActiveItem] = React.useState(0);
-
-  const drawerData = [
+  const cardData = [
     {
-      id: 'ex-toggle1',
-      name: 'Get started with the Hybrid Cloud Console',
-      img: '/apps/frontend-assets/console-landing/widget-explore/Explore_Get-started.svg',
-      title: 'Take a tour of the Console',
-      body: "There's a lot to explore in the Hybrid Cloud Console, and understanding its capabilities will increase your efficiency.",
-      buttonName: 'Start the guided tour',
+      id: 'card1',
+      img: '/apps/frontend-assets/console-landing/widget-explore/logo_hat-only.svg',
+      title: 'Get started with a tour',
+      body: "Take a quick guided tour to understand how the Red Hat Hybrid Cloud Console's capabilities will increase your efficiency.",
       ouiaId: 'start-guided-tour-button',
+      url: '',
     },
     {
-      id: 'ex-toggle2',
-      name: 'Try OpenShift on AWS',
-      img: '/apps/frontend-assets/console-landing/widget-explore/Explore_ROSA.svg',
-      title: 'Get started with Red Hat OpenShift Service on AWS (ROSA)',
-      body: 'Quickly build, deploy, and scale applications with our fully-managed turnkey application platform.',
-      buttonName: 'Try ROSA',
+      id: 'card2',
+      img: '/apps/frontend-assets/console-landing/widget-explore/Logo-Red_Hat-AWS-A-Standard-RGB.svg',
+      title: 'Try OpenShift on AWS',
+      body: 'Quickly build, deploy, and scale applications with Red Hat OpenShift Service on AWS (ROSA), our fully-managed turnkey application platform.',
       ouiaId: 'try-rosa-button',
       url: '/openshift/overview/rosa',
     },
     {
-      id: 'ex-toggle3',
-      name: 'Develop on the OpenShift Sandbox',
-      img: '/apps/frontend-assets/console-landing/widget-explore/Explore_sandbox.svg',
-      title: 'Develop in the sandbox with the Red Hat Developer program',
-      body: "Try Red Hat's products and technologies without setup or configuration.",
-      buttonName: 'Explore the sandbox',
+      id: 'card3',
+      img: '/apps/frontend-assets/console-landing/widget-explore/Logo-Red_Hat-Developer-A-Standard-RGB.svg',
+      title: 'Develop in the Sandbox',
+      body: "Try Red Hat's products and technologies without setup or configuration with the Development Sandbox for Red Hat OpenShift.",
       ouiaId: 'explore-sandbox-button',
       url: '/openshift/sandbox',
     },
     {
-      id: 'ex-toggle4',
-      name: 'Analyze your envionments',
+      id: 'card4',
       img: '/apps/frontend-assets/console-landing/widget-explore/Explore_Insights-analyze.svg',
-      title: 'Continuously analyze with Red Hat Insights',
+      title: 'Analyze RHEL environments',
       body: 'Analyze platforms and applications from the console to better manage your hybrid cloud environments.',
-      buttonName: 'Identify and resolve risks',
       ouiaId: 'analyze-risk-button',
       url: '/insights/dashboard#SIDs=&tags=',
     },
     {
-      id: 'ex-toggle5',
-      name: 'Connect to subscriptions',
-      img: '/apps/frontend-assets/console-landing/widget-explore/Explore_subs.svg',
-      title: 'Empower your buying decisions with data',
-      body: 'Subscription Services provides reporting to help you make data-driven subscription choices.',
-      buttonName: 'Explore subscriptions',
-      ouiaId: 'explore-button',
-      url: '/insights/subscriptions/inventory#SIDs=&tags=',
-    },
-    {
-      id: 'ex-toggle6',
-      name: 'Convert your CentOS systems to RHEL',
+      id: 'card5',
       img: '/apps/frontend-assets/console-landing/widget-explore/Explore_CentOS-to-RHEL.svg',
-      title: 'Convert your CentOS systems to Red Hat Enterprise Linux',
-      body: (
-        <div>
-          <p>
-            On June 30, 2024, CentOS Linux 7 will reach End of Life (EOL), and
-            those systems will stop receiving updates, security patches, and new
-            features.
-          </p>
-          <p>
-            Red Hat can help.{' '}
-            <a
-              href="https://www.redhat.com/en/technologies/linux-platforms/enterprise-linux/centos-migration"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Learn more
-            </a>{' '}
-            about migrating your CentOS Linux systems to RHEL, whether
-            on-premise or in the cloud.
-          </p>
-        </div>
-      ),
-      buttonName: 'Run a pre-conversion analysis',
+      title: 'Convert from CentOS to RHEL',
+      body: 'CentOS Linux 7 has reached End of Life (EOL). Prep your systems from conversion from CentOS to RHEL.',
       ouiaId: 'cent-os-button',
       url: '/insights/tasks/available/convert-to-rhel-analysis?quickstart=insights-tasks-pre-conversion',
     },
-    {
-      id: 'ex-toggle7',
-      name: 'Configure your console',
-      img: '/apps/frontend-assets/console-landing/widget-explore/Explore_configure.svg',
-      title: 'Customize your notification settings',
-      body: 'Opt-in and out of receiving notifications for your console services.',
-      buttonName: 'Configure settings',
-      ouiaId: 'configure-settings-button',
-      url: '/settings/notifications',
-    },
   ];
 
-  const panelContent = (
+  const cardContent = cardData.map((cardData) => (
     <>
-      <DrawerPanelContent
-        key={drawerData[activeItem].id}
-        colorVariant="no-background"
-      >
-        <DrawerPanelBody className="pf-v5-u-display-flex pf-v5-u-flex-direction-row">
-          <Card
-            className="pf-v5-u-align-self-stretch pf-v5-u-flex-fill"
-            isPlain
-          >
-            <CardBody className="pf-v5-u-p-0 ">
+      <div key={cardData.id} className="pf-v5-u-m-0">
+        <Link to={cardData.url}>
+          <Card isFullHeight isSelectableRaised>
+            <CardBody className="pf-v5-u-p-md pf-v5-u-text-align-center land-c-card__body-explore">
+              <img
+                className="pf-v5-u-justify-content-center"
+                src={cardData.img}
+              />
               <TextContent>
-                <Text component="p" className="title pf-v5-u-mb-sm">
-                  {drawerData[activeItem].title}
-                </Text>
                 <Text
                   component="p"
-                  className="pf-v5-u-font-size-sm pf-v5-u-mb-sm"
+                  className="pf-v5-u-mb-0 pf-v5-u-font-size-sm"
                 >
-                  {drawerData[activeItem].body}
+                  {cardData.title}
+                </Text>
+                <Text component="small" className="pf-v5-u-font-size-xs">
+                  {cardData.body}
                 </Text>
               </TextContent>
-              <Button
-                component="a"
-                size="lg"
-                href={drawerData[activeItem].url}
-                className="pf-v5-u-mb-sm cta-button"
-                ouiaId={drawerData[activeItem].ouiaId}
-              >
-                {drawerData[activeItem].buttonName}
-              </Button>
             </CardBody>
           </Card>
-          <img
-            className="widg-explore-image pf-v5-u-align-self-flex-start pf-v5-u-flex-none pf-v5-u-m-lg"
-            src={drawerData[activeItem].img}
-          />
-        </DrawerPanelBody>
-      </DrawerPanelContent>
+        </Link>
+      </div>
     </>
-  );
-
-  const drawerContent = (
-    <SimpleList>
-      <SimpleListItem isActive onClick={() => setActiveItem(0)}>
-        Get started with the Hybrid Cloud Console
-      </SimpleListItem>
-      <SimpleListItem onClick={() => setActiveItem(1)}>
-        Try OpenShift with AWS
-      </SimpleListItem>
-      <SimpleListItem onClick={() => setActiveItem(2)}>
-        Develop on the OpenShift Sandbox
-      </SimpleListItem>
-      <SimpleListItem onClick={() => setActiveItem(3)}>
-        Analyze your environments
-      </SimpleListItem>
-      <SimpleListItem onClick={() => setActiveItem(4)}>
-        Connect to your subscriptions
-      </SimpleListItem>
-      <SimpleListItem onClick={() => setActiveItem(5)}>
-        Convert your CentOS systems to RHEL
-      </SimpleListItem>
-      <SimpleListItem onClick={() => setActiveItem(6)}>
-        Configure your console
-      </SimpleListItem>
-    </SimpleList>
-  );
+  ));
 
   return (
     <React.Fragment>
-      <Drawer className="widget-explore" isStatic>
-        <DrawerContent panelContent={panelContent}>
-          <DrawerContentBody>{drawerContent}</DrawerContentBody>
-        </DrawerContent>
-      </Drawer>
+      <Gallery className="widget-explore pf-v5-u-p-md">{cardContent}</Gallery>
     </React.Fragment>
   );
 };
