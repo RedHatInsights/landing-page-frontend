34df2e7bb82c4abc5266471004a2e1ef675d1a2a
Stephen <tsadams@gmail.com>
2025-02-07 12:20:46 -0500

    Apply suggestions from code review
    Co-authored-by: Karel Hala <khala@redhat.com>

--- src/routes/Landing.tsx
+++ src/routes/Landing.tsx
@@ -3,14 +3,14 @@ import { useFlag } from '@unleash/proxy-client-react';
 import { ScalprumComponent } from '@scalprum/react-core';
 import useChrome from '@redhat-cloud-services/frontend-components/useChrome';
 
-const Landing = ({ layoutType }: { layoutType?: string }) => {
+const Landing: React.FC<{ layoutType?: string }> = ({ layoutType }) => {
   const scope = 'widgetLayout';
   const { isBeta } = useChrome();
   const widgetLayoutLandingPageEnabled =
     (isBeta() && useFlag('platform.landing-page.widgetization')) ||
     (!isBeta() && useFlag('platform.landing-page.widgetization-stable'));
   const props = {
-    ...(layoutType && {layoutType: layoutType})
+    ...(layoutType && { layoutType: layoutType }),
   };
   return (
     <Fragment>
