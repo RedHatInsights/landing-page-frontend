037c298c93abf39ede19fc16e5549252634e038f
alex <aferdina@redhat.com>
2025-10-23 08:09:39 -0400

    chore: update text for lightspeed rebrand

--- src/components/widgets/rhel-widget.tsx
+++ src/components/widgets/rhel-widget.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { SimpleServiceWidget } from './simple-service-widget';
+import { useFlag } from '@unleash/proxy-client-react';
 
 const RhelWidget: React.FunctionComponent = () => {
+  const lightSpeedRebrand = useFlag('platform.lightspeed-rebrand');
+
   return (
     <>
       <SimpleServiceWidget
         id={0}
         body="Proactively assess, secure, and stabilize the business-critical services that you scale from your RHEL systems."
-        linkTitle="Insights for RHEL"
+        linkTitle={lightSpeedRebrand ? 'RHEL' : 'Insights for RHEL'}
         url="/insights/"
       />
     </>
